d5c1d451905bcc7bd0af609a90de246720219da9
LeeGwangYong <rhkdrmfh@gmail.com>
2019-01-17 15:45:56 -0500

    Initial Commit

---
+++ MenuWebView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuWebView
+//
+//  Created by United Merchant Services.inc on 1/17/19.
+//  Copyright © 2019 GY. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuWebView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuWebView
+//
+//  Created by United Merchant Services.inc on 1/17/19.
+//  Copyright © 2019 GY. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
