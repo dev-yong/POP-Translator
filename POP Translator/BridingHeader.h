5592250561897364cf99d134350429edbc08867d
이광용 <rhkdrmfh@gmail.com>
2019-01-27 19:53:04 -0500

    Remove Carthage, Apply MASShourtcut

---
+++ POP Translator/BridingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridingHeader.h
+//  POP Translator
+//
+//  Created by 이광용 on 27/01/2019.
+//  Copyright © 2019 GY. All rights reserved.
+//
+
+#ifndef BridingHeader_h
+#define BridingHeader_h
+#import <Cocoa/Cocoa.h>
+#import <MASShortcut/Shortcut.h>
+
+#endif /* BridingHeader_h */
